9d786b9177ba52c98c87c8a68da379c62baf95c7
Naga Sushma Maddirala <sushma.maddirala@wavemaker.com>
2018-07-16 08:29:53 +0000

    testpush1 New files added - [AdventureWorks2014,CountryInfoService,FeedService,FileService,MyJavaService,googleapis,heartRate,en.json] Files modified - [Common,Login,Main,footer,header,leftnav,rightnav,topnav,hrdb,securityService,WEB-INF,data,lib,logos,momentLocale,ngLocale,pages,profiles,resources,webapp,.gitignore,.wmproject.properties,README.md,pom.xml,README.md,Readme.txt,Readme.txt,README.md]

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,15 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "testPermission",
+  "homePage" : "Main",
+  "name" : "testPermission",
+  "platformType" : "WEB",
+  "securityEnabled" : "true",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0",
+  "xsrf_header_name" : "X-WM-XSRF-TOKEN"
+};
